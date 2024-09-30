4eadb303635109f42e21d3e7452c747eb2eda7a7
Cesar Fernandez <CesarFernandez2@live.com>
2024-09-30 10:12:25 -0600

    initial homeViewController finished

--- mindfulness/ViewControllers/HomeViewController.swift
+++ mindfulness/ViewControllers/HomeViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    var breathExercise: BreathExercise
+        
     @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBOutlet weak var boxBeginnerBttn: UIButton!
+    @IBOutlet weak var boxIntermediateBttn: UIButton!
+    @IBOutlet weak var boxAdvancedBttn: UIButton!
+    
+    @IBOutlet weak var resonanceBeginnerBttn: UIButton!
+    @IBOutlet weak var resonanceIntermediateBttn: UIButton!
+    @IBOutlet weak var resonanceAdvacedBttn: UIButton!
+    
+    @IBOutlet weak var fourSevenEightBeginnerBttn: UIButton!
+    @IBOutlet weak var fourSevenEightIntermediateBttn: UIButton!
+    @IBOutlet weak var fourSevenEightAdvancedBttn: UIButton!
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +35,62 @@ class HomeViewController: UIViewController {
                 quoteLabel.text = "\(quote.q)\n-\(quote.a)"
             }
         }
+        setBoxBreathLevel()
+        setResonanceBreathLevel()
+        setFourSevenEightBreathLevel()
     }
     
+    func setBoxBreathLevel() {
+        if boxBeginnerBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.box
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.beginner
+        }
+        
+        if boxIntermediateBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.box
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.intermediate
+        }
+        
+        if boxAdvancedBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.box
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.advanced
+        }
+    }
 
+    func setResonanceBreathLevel() {
+        if resonanceBeginnerBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.resonance
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.beginner
+        }
+        
+        if resonanceIntermediateBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.resonance
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.intermediate
+        }
+        
+        if resonanceAdvacedBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.resonance
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.advanced
+        }
+        
+        }
+    
+    func setFourSevenEightBreathLevel() {
+        if fourSevenEightBeginnerBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.fourSevenEight
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.beginner
+        }
+        
+        if fourSevenEightIntermediateBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.fourSevenEight
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.intermediate
+        }
+        
+        if fourSevenEightAdvancedBttn.isEnabled {
+            breathExercise.typeOfBreathExercise = BreathExercise.TypeOfBreathExercise.fourSevenEight
+            breathExercise.exerciseDuration = BreathExercise.ExerciseDuration.advanced
+        }
+    }
     /*
     // MARK: - Navigation
 
